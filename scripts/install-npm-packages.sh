99657cce3bba0584cb368c73516be44872828e99
Nicholas Merritt <nimerritt@gmail.com>
2017-06-11 08:10:23 +0200

    add lerna to global install

--- scripts/install-npm-packages.sh
+++ scripts/install-npm-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source $HOME/.bashrc 
 echo Using npm version $(npm -v) with node $(node -v).
-npm install -g typescript ts-node tslint
+npm install -g typescript ts-node tslint lerna
 
 
