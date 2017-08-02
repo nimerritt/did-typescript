66826393d4d1792126a3846f318d483acfab31c8
Nicholas Merritt <nimerritt@gmail.com>
2017-08-02 15:43:44 +0200

    add yarn

--- scripts/install-npm-packages.sh
+++ scripts/install-npm-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source $HOME/.bashrc 
 echo Using npm version $(npm -v) with node $(node -v).
-npm install -g typescript ts-node tslint lerna
+npm install -g typescript ts-node tslint lerna yarn
 
 
